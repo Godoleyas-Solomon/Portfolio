2a94b14872ee1dc5f4cfc30d7b4c5bb042f6729f
BuildTools <itsmepapy@gmail.com>
2023-08-12 13:28:10 +0300

    Final Upload

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site_name: 'Godoleyas\'s Portfolio',
-  title: 'Godoleyas\'s Portfolio',
-  description: 'My portfolio website',
-  author: 'Godoleyas',
-  locale_region: 'en-us',
-  locale: 'en',
-};
